ad2a73eb32eea646541fde03b44270dcf90280c0
Gaoussou9 <gaoussoucisse07@gmail.com>
2026-04-08 13:14:24 +0000

    fix(api): use production API URL for ads service

--- src/components/Ads/adsService.js
+++ src/components/Ads/adsService.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL + "/api/ads";
 
 export const fetchAds = async (params = {}) => {
   const cleanParams = Object.fromEntries(
